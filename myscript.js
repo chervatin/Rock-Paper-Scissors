41bdf6c860109f9af48c966e18b57b40688f8d84
Chervatin <jmchervatin@gmail.com>
2022-05-21 19:30:16 +0200

    Add game working. Pending changes on interface

--- myscript.js
+++ myscript.js
@@ -1,64 +1,87 @@
-let computerSelection;
-let playerSelectionDirect = "Rock";
-let playerSelection = playerSelectionDirect.toLowerCase();
-let playerPoints = 0;
-let computerPoints = 0;
 
- function computerPlay(){
+// Creates the <div> position were later the results are shown
+var container = document.querySelector('#container');
+var content = document.createElement('div');
+content.textContent = 'Lets play!';
+container.appendChild(content);    
+
+// buttons is a node list. It looks and acts much like an array
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
     
-    let options = [
-        "rock",
-       "paper",
-        "scissor"
-      ];
-      computerSelection = options[Math.floor(Math.random() * (options.length))];
-}
-
-computerPlay();
-console.log(computerSelection)
-console.log(playerSelection)
-
-
-function playRound(computerSelection,playerSelection){
-    switch(true){
-    case computerSelection === playerSelection:
-        console.log("It is a tie!");
-        break
-    case (computerSelection === "rock" && playerSelection === "paper"):
-        console.log("You Win! Paper beats rock")
-        playerPoints = playerPoints + 1;
-        break
-    case (computerSelection === "rock" && playerSelection === "scissor"):
-        console.log("You Lose! :( Rock beats scissor");
-        computerPoints = computerPoints + 1;
-        break
-    case (computerSelection === "paper" && playerSelection === "rock"):
-        console.log("You Lose! :( Paper beats Rock");
-        computerPoints = computerPoints + 1;
-        break
-    case (computerSelection === "paper" && playerSelection === "scissor"):
-        console.log("You Win! Scissor beats paper");
-        playerPoints = playerPoints + 1;
-        break
-    case (computerSelection === "scissor" && playerSelection === "paper"):
-        console.log("You Lose :( Scissor beats paper");
-        computerPoints = computerPoints + 1;
-        break
-    case (computerSelection === "scissor" && playerSelection === "rock"):
-        console.log("You Win! Rock beats Scissor");
-        playerPoints = playerPoints + 1;
-        break
-}
-}
-
-function game(){
-    for (let i = 0; i < 5; i++) {
-        computerPlay();
-        let playerSelectionDirect = prompt('Choose between Rock, Paper or Scissor');
-        let playerSelection = playerSelectionDirect.toLowerCase();
-        playRound(computerSelection,playerSelection);
-        console.log(playerPoints)
-     }
+    function computerPlay(){
     
-}
-game();
+        let options = [
+            "rock",
+           "paper",
+            "scissor"
+          ];
+          computerSelection = options[Math.floor(Math.random() * (options.length))];
+    }
+    let computerSelection;
+    let playerSelection;
+    let playerPoints = 0;
+    let computerPoints = 0;
+    
+    computerPlay();
+    playerSelection = button.id;
+
+    function playRound(computerSelection,playerSelection){
+        switch(true){
+        case computerSelection === playerSelection:
+            content.textContent = 'It is a tie!';
+            break
+        case (computerSelection === "rock" && playerSelection === "paper"):
+            content.textContent = 'You Win! Paper beats rock';
+            playerPoints = playerPoints + 1;
+            break
+        case (computerSelection === "rock" && playerSelection === "scissor"):
+            content.textContent = 'You Lose! :( Rock beats scissor';
+            computerPoints = computerPoints + 1;
+            break
+        case (computerSelection === "paper" && playerSelection === "rock"):
+            content.textContent = 'You Lose! :( Paper beats Rock';
+            computerPoints = computerPoints + 1;
+            break
+        case (computerSelection === "paper" && playerSelection === "scissor"):
+            content.textContent = 'You Win! Scissor beats paper';
+            playerPoints = playerPoints + 1;
+            break
+        case (computerSelection === "scissor" && playerSelection === "paper"):
+            content.textContent = 'You Lose :( Scissor beats paper';
+            computerPoints = computerPoints + 1;
+            break
+        case (computerSelection === "scissor" && playerSelection === "rock"):
+            content.textContent = 'You Win! Rock beats Scissor';
+            playerPoints = playerPoints + 1;
+            break
+    }
+    
+    }
+    playRound(computerSelection,playerSelection) 
+  });
+});
+
+
+
+
+
+
+
+
+//function game(){
+    //for (let i = 0; i < 5; i++)//
+        //computerPlay();
+        //let playerSelectionDirect = prompt('Choose between Rock, Paper or Scissor');
+       // let playerSelection = playerSelectionDirect.toLowerCase();
+        //playRound(computerSelection,playerSelection);
+        //console.log(playerPoints)
+     //}
+    
+
+//game();
